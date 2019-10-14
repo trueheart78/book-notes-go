87ac2b015f25b919cf50ada086bcb0132fa1d762
Josh Mills <josh@trueheart78.com>
2019-10-14 12:17:33 -0400

    test clarifications

--- internal/pkg/adjective/adjective_test.go
+++ internal/pkg/adjective/adjective_test.go
@@ -6,9 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestSample description
-func TestSample(t *testing.T) {
+// TestAdjectiveGet tests that the return value is not nil
+func TestAdjectiveGet(t *testing.T) {
 	assert := assert.New(t)
-	// assert equality
 	assert.NotNil(Get(), "should return a value")
 }
